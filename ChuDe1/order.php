7b0fc28ab3293644ae5ed4dbd6b2523f1dfb4e6d
diennguyen2611 <you@example.com>
2021-09-19 23:15:45 +0700

    Sửa tiêu đề và footer

--- ChuDe1/order.php
+++ ChuDe1/order.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Order</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
@@ -111,7 +111,7 @@
         <div>
         <img src="icons/logo_twitter.png" alt="" width="32px" >
         </div>
-        <p>All rights reserved. Designed by <span><a href="">CSE.TLU</a></span></p>
+        <p>All rights reserved. Designed by <span><a href="">NguyenThiDien</a></span></p>
 
     </div>
 
